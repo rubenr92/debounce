a195ad8623ae1e80e8bb84f3a6de4a09beeecd29
Ruben <rubenr28@outlook.es>
2024-06-19 10:34:23 +0200

    throttle function

--- src/throttle.ts
+++ src/throttle.ts
@@ -6,10 +6,9 @@ export default function throttle(mainFunction:any, delay:number) {
     return (...args:any[]) => {
       if (timerFlag === null) { 
         mainFunction(...args); 
-        timerFlag = setTimeout(() => { // Set a timer to clear the timerFlag after the specified delay
-          timerFlag = null; // Clear the timerFlag to allow the main function to be executed again
+        timerFlag = setTimeout(() => {
+          timerFlag = null; 
         }, delay);
       }
-      //console.log(timerFlag)
     };
   }
